3f7bcb6a8d439cf81d3f635cd24d1bcd6ec0bb66
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:16:33 +0800

    Updated for 2.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 Undo Redo for Meteor
 ====================
 
+### v4.1 & v4.2
+
+- Version bump as per 1.8.2 instructions in [Meteor changelog](https://github.com/meteor/meteor/blob/devel/History.md)
+
 ### v4.0 
 
 - Added support for filtering transactions by context (pass `contextSelector` as a widget param and set it equal to an object with key/value pairs)
--- package.js
+++ package.js
@@ -1,31 +1,29 @@
 Package.describe({
   name: 'babrahams:undo-redo',
-  version: '0.4.0',
+  version: '0.4.3',
   summary: 'Undo-redo widget for babrahams:transactions',
   git: 'https://github.com/JackAdams/meteor-undo-redo.git',
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use('jquery', 'client');
+Package.onUse(function (api) {
+  api.versionsFrom(['1.8.2', '2.3']);
   api.use('tracker', 'client');
   api.use('minimongo', 'client');
-  api.use('templating', 'client');
-  api.use('spacebars', 'client');
+  api.use(['templating@1.3.2', 'spacebars@1.0.15', 'jquery@1.11.11'], 'client');
   api.use('underscore');
   api.use('mongo');
   api.imply('mongo');
   api.use('accounts-base');
-  api.use('babrahams:transactions@0.8.8');
+  api.use('babrahams:transactions@0.8.11');
   api.imply('babrahams:transactions');
-  api.addFiles('lib/undo_redo_client.css','client');
-  api.addFiles('lib/undo_redo_client.html','client');
-  api.addFiles('lib/undo_redo_client.js','client');
-  api.addFiles('lib/undo_redo_server.js','server');
+  api.addFiles('lib/undo_redo_client.css', 'client');
+  api.addFiles('lib/undo_redo_client.html', 'client');
+  api.addFiles('lib/undo_redo_client.js', 'client');
+  api.addFiles('lib/undo_redo_server.js', 'server');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('babrahams:undo-redo');
   api.addFiles('undo_redo_tests.js');
